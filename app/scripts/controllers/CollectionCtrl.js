ead751278bf9841bebdc1a3185d1931cf0cdb52a
Curtis Wilson <curtiswemail@gmail.com>
2017-11-15 22:36:33 -0700

    Finish Debug. Start assignment

--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -1,9 +1,10 @@
 (function(){
-  function CollectionCtrl{
-    this.album = [];
-    for(var i =0; i< 12; i++){
-      this.albums.push(angular.copy(albumPicassio));
+  function CollectionCtrl(){
+    this.albums = [];
+    for(var i =0; i < 12; i++){
+      this.albums.push(angular.copy(albumPicasso));
     }
+  
 }
   angular
   .module('blocJams')
