a346cacd342e9cc8eb5d51158e7e9b518179aa19
William C Wilson <curtiswemail@gmail.com>
2017-11-20 15:31:44 -0700

    Finish checkpoint 7. Start checkpoint 7 assignment

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -3,19 +3,25 @@
     var SongPlayer = {};
 
     var currentSong = null;
+
+  /**
+  * @desc Buzz object audio file
+  * @type {object}
+  */
     var currentBuzzObject = null;
 
-    SongPlayer.play = function(song) {
-      if (currentSong !== song){
-        if (currentBuzzObject){
-          currentBuzzObject.stop();
-          currentSong.playing = null;
+/**
+* @function setSong
+* @desc Stops currently playing song and loads new audio file as currentBuzzObject
+* @param {object} song
+*/
 
-        }else if (currentSong === song){
-          if (currentBuzzObject.isPaused()){
-            currentBuzzObject.play();
-          }
-        }
+    var setSong = function (song) {
+      if (currentBuzzObject){
+        currentBuzzObject.stop();
+        currentSong.playing = null;
+
+      }
 
       currentBuzzObject = new buzz.sound(song.audioUrl, {
         formats:['mp3'],
@@ -24,9 +30,23 @@
 
       currentSong = song;
 
+    };
+
+    SongPlayer.play = function(song) {
+      if (currentSong !== song){
+      setSong(song);
       currentBuzzObject.play();
       song.playing = true;
+    }else if (currentSong === song){
+      if (currentBuzzObject.isPaused()){
+        currentBuzzObject.play();
+      }
     }
+  };
+
+  SongPlayer.pause = function(song) {
+    currentBuzzObject.pause();
+    song.playing = false;
   };
 
     return SongPlayer;
